c401693f2f917c2f728be7a8f7d75757740295c2
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-09-01 19:02:51 +0800

    feat: add config options, generateProgress and togglePause functions to publick API

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,89 @@
-import style from './styles/$.scss';
-import { useState, useCallback, useEffect } from 'haunted';
+import { useState, useEffect } from 'haunted';
 import { FC } from '@web-companions/fc';
+import createEmotion, { Emotion } from 'create-emotion';
 
-export const loadingProgressBarEl = FC(function (prop: { ref?: { current?: any } } = { ref: { current: {} } }) {
-  const [animationName, setAnimationName] = useState('f0');
+import style from './styles/$.scss';
+
+interface LoadingProgressBarProps {
+  ref?: { current?: any };
+  config?: {
+    duration?: number; // ms (2000ms by default)
+    stepsCount?: number; // (1 by default)
+  };
+}
+
+export const loadingProgressBarEl = FC(function (props: LoadingProgressBarProps = { ref: { current: {} } }) {
+  const [config, setConfig] = useState({
+    duration: 2000,
+    stepsCount: 1,
+    ...props.config,
+  });
+  const [frames, setFrames] = useState(undefined);
   const [isPause, setIsPause] = useState(false);
+  const [emotionStylesTpl, setEmotionStylesTpl] = useState<HTMLTemplateElement>(undefined);
+  const [{ css, keyframes }] = (useState(() => {
+    let emotion;
+    if (this.shadowRoot !== null) {
+      const emotionTemplateContainer = document.createElement('template');
+      emotionTemplateContainer.setAttribute('id', 'data-emotion');
+      emotion = createEmotion({ container: emotionTemplateContainer });
+      setEmotionStylesTpl(emotionTemplateContainer);
+    } else {
+      emotion = createEmotion();
+    }
+
+    return emotion;
+  }) as unknown) as [Emotion];
 
   useEffect(() => {
+    const _config = {
+      duration: 2000,
+      stepsCount: 1,
+      ...props.config,
+    };
+    setConfig(_config);
+
+    let index = 0;
     const generator = function* () {
-      yield setAnimationName('f1');
-      yield setAnimationName('f2');
-      yield setAnimationName('f3');
-      yield setAnimationName('f4');
-      yield setAnimationName('f5');
-      yield setAnimationName('f6');
-      yield setAnimationName('f7');
-      yield setAnimationName('f8');
-      yield setAnimationName('f9');
-      yield setAnimationName('f10');
+      while (true) {
+        if (index < _config.stepsCount) {
+          const k = 100 / _config.stepsCount;
+          setFrames(keyframes`
+            from {
+              width: ${index * k}%;
+            }
+            to {
+              width: ${(index + 1) * k}%;
+            }      
+          `);
+
+          yield ++index;
+        } else {
+          setIsPause(false);
+          setFrames(undefined);
+          index = 0;
+          yield index;
+        }
+      }
     };
 
-    prop.ref = prop.ref ?? {};
-    prop.ref.current = this ?? {};
-    prop.ref.current.generateProgress = generator();
-  }, []);
-
-  const handlePause = useCallback(() => {
-    setIsPause(!isPause);
-  }, [isPause]);
+    props.ref = props.ref ?? {};
+    props.ref.current = this ?? {};
+    props.ref.current.generateProgress = generator();
+    props.ref.current.togglePause = (pause: boolean) => setIsPause((hasPause) => (typeof pause === 'boolean' ? pause : !hasPause));
+  }, [props.ref, props.config]);
 
   return (
     <>
       <style>{style}</style>
-      <div class={`animated yt-loader ${isPause ? 'pause' : ''}`} style={`animation-name: ${animationName}`}></div>
+      {emotionStylesTpl}
+      <div
+        class={`yt-loader ${css`
+          animation-name: ${frames};
+          animation-duration: ${config.duration}ms;
+          animation-play-state: ${isPause ? 'paused' : 'running'};
+        `}`}
+      ></div>
     </>
   );
 }).element;
