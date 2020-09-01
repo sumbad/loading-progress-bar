1afd41e5f58dea26b32871f5f053e9a81aa12860
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-09-01 19:03:12 +0800

    chore: update demo

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useCallback } from 'haunted';
+import { useRef, useCallback, useState } from 'haunted';
 import { FC } from '@web-companions/fc';
 import { loadingProgressBarEl } from '../../src';
 
@@ -8,23 +8,51 @@ const LoadingProgressBar = loadingProgressBarEl('loading-progress-bar', { useSha
  * ROOT element
  */
 FC(() => {
-  const myRef = useRef<{ generateProgress?: Generator }>({});
+  const myRef = useRef<{
+    generateProgress?: Generator;
+    togglePause?: (isPause?: boolean) => void;
+  }>({});
+  const [loaderConfig, setLoaderConfig] = useState({
+    duration: 2000,
+    stepsCount: 1,
+  });
 
   const handleProgress = useCallback(() => {
     if (myRef.current.generateProgress !== undefined) {
       const r = myRef.current.generateProgress.next();
+      if (r.value === 1) {
+        setTimeout(() => {
+          myRef.current.togglePause(true);
+        }, 300);
+        setTimeout(() => {
+          myRef.current.togglePause(false);
+        }, 1000);
+        setTimeout(() => {
+          myRef.current.togglePause(true);
+        }, 2000);
+        setTimeout(() => {
+          myRef.current.togglePause(false);
+        }, 3000);
+      }
       console.log(JSON.stringify(r));
     }
   }, []);
 
   return (
     <>
-      <LoadingProgressBar ref={myRef}></LoadingProgressBar>
-      {/* <loading-progress-bar ref={myRef}></loading-progress-bar> */}
+      <LoadingProgressBar config={loaderConfig} ref={myRef}></LoadingProgressBar>
       <br />
       <button onclick={handleProgress}>Progress loading</button>
       <br />
-      <h1>Hello!</h1>
+      <button onclick={() => myRef.current?.togglePause()}>Pause/Run</button>
+      <br />
+      <textarea
+        rows="10"
+        value={JSON.stringify(loaderConfig, null, 2)}
+        onchange={(e: Event) => {
+          setLoaderConfig(JSON.parse((e.currentTarget as HTMLTextAreaElement).value));
+        }}
+      ></textarea>
     </>
   );
 }).element('demo-app');
