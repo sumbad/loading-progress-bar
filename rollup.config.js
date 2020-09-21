ed26419f47b4e18d549d09aed1656f92fbebed46
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-09-21 10:59:14 +0800

    chore: add minifiers to a bundling process

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import { dev, prod } from './base.rollup.config';
-
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
-import analyze from 'rollup-plugin-analyzer'
+import analyze from 'rollup-plugin-analyzer';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import minifyTaggedCSSTemplate from 'rollup-plugin-minify-tagged-css-template';
 const terser = require('rollup-plugin-terser').terser;
-const minifyLiteralsHTMLPlugin = require('rollup-plugin-minify-html-literals').default;
 
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 
@@ -17,22 +17,60 @@ const atImport = require('postcss-import');
 const production = !process.env.ROLLUP_WATCH;
 
 const plugins = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('development'),
+  }),
+  resolve(),
   typescriptPlugin({
     tsconfig: `./tsconfig.${production ? 'prod' : 'dev'}.json`,
   }),
   babel({
     babelHelpers: 'bundled',
     extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-    exclude: 'node_modules/**'
+    exclude: 'node_modules/**',
   }),
   postcss({
     inject: false,
     extensions: ['.css', '.pcss', '.scss'],
     plugins: [atImport()],
   }),
-  analyze(),
 ];
 
+export const dev = (input, file, plugins) => ({
+  input,
+  output: {
+    file,
+    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
+  },
+  plugins,
+});
+
+export const prod = (input, file, plugins) => ({
+  input,
+  output: {
+    file,
+    format: 'es',
+  },
+  plugins: [
+    ...plugins,
+    minifyHTML(),
+    minifyTaggedCSSTemplate({
+      parserOptions: {
+        sourceType: 'module', // treat files as ES6 modules
+        plugins: [
+          'syntax-dynamic-import', // handle dynamic imports
+          [
+            'decorators', // use decorators proposal plugin
+            { decoratorsBeforeExport: true },
+          ],
+        ],
+      },
+    }),
+    terser(),
+    analyze(),
+  ],
+});
 
 export const prodIife = (input, file, plugins) => ({
   input,
@@ -40,14 +78,28 @@ export const prodIife = (input, file, plugins) => ({
     file,
     format: 'iife',
   },
-  plugins: [resolve(), terser(), minifyLiteralsHTMLPlugin(), ...plugins],
-  // indicate which modules should be treated as external
-  // external: ['@web-companions/fc', 'haunted', 'lit-html'],
+  plugins: [
+    ...plugins,
+    minifyHTML(),
+    minifyTaggedCSSTemplate({
+      parserOptions: {
+        sourceType: 'module', // treat files as ES6 modules
+        plugins: [
+          'syntax-dynamic-import', // handle dynamic imports
+          [
+            'decorators', // use decorators proposal plugin
+            { decoratorsBeforeExport: true },
+          ],
+        ],
+      },
+    }),
+    terser(),
+  ],
 });
 
 export default production
   ? [
-      prod('./src/index.tsx', './lib/index.js', plugins), 
-      // prodIife('./src/loading-progress-bar.tsx', './lib/loading-progress-bar.js', plugins)
+      prod('./src/index.tsx', './lib/index.js', plugins),
+      prodIife('./src/loading-progress-bar.tsx', './lib/loading-progress-bar.js', plugins),
     ]
   : dev('./www/src/index.tsx', './www/index.js', plugins);
